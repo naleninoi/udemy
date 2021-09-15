2cb7e0c66028b02ff52f08a3e33db0fdb0e87f8f
Viktor K-A <naleninoi@gmail.com>
2021-09-15 20:41:57 +0300

    App created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RootComponent } from './components/common/root/root.component';
+import { HeadComponent } from './components/common/head/head.component';
+import { ListViewComponent } from './components/shopping-list/list-view/list-view.component';
+import { ListEditComponent } from './components/shopping-list/list-edit/list-edit.component';
+import { RecipeListComponent } from './components/recipe-book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './components/recipe-book/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './components/recipe-book/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RootComponent,
+    HeadComponent,
+    ListViewComponent,
+    ListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule
